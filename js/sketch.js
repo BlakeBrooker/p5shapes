5918239751aa33fedce50f0d81920c7e80c37bf5
BlakeBrooker <116875628+BlakeBrooker@users.noreply.github.com>
2022-10-28 10:19:47 +0100

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,18 @@
+//setup function runs once at  the start
 function setup() {
     var canvas = createCanvas(800, 800);
     canvas.parent("p5container");
 }
 
+//draw function is a continuos loop, runs from the start, at 60fps
 function draw() {
-    background(220);
+    background(220); //light grey background
 
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    fill(255, 255, 255); //setting fill to rgb value for white
+    ellipse(400, 400, 200,); //circle being drawn on our canvas
+
+
+ 
+    fill(200, 845, 213,);
+    square(300, 400, 200);
 }
