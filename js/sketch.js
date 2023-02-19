74c33fbde59f8f2afc9b2106f668fcc770cc67f4
Blake <116875628+BlakeBrooker@users.noreply.github.com>
2023-02-19 12:40:55 +0000

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -1,18 +1,20 @@
-//setup function runs once at  the start
+let angle = 0;
+let stepSize = 5;
+
 function setup() {
-    var canvas = createCanvas(800, 800);
-    canvas.parent("p5container");
+  createCanvas(500, 500);
+  colorMode(HSB, 360, 100, 100);
+  noStroke();
 }
 
-//draw function is a continuos loop, runs from the start, at 60fps
 function draw() {
-    background(220); //light grey background
-
-    fill(255, 255, 255); //setting fill to rgb value for white
-    ellipse(400, 400, 200,); //circle being drawn on our canvas
-
-
- 
-    fill(200, 845, 213,);
-    square(300, 400, 200);
+  background(0);
+  for (let radius = 10; radius < width/2; radius += 30) {
+    let xPos = width/2 + cos(angle) * radius;
+    let yPos = height/2 + sin(angle) * radius;
+    fill((xPos+yPos) % 360, 80, 100);
+    ellipse(xPos, yPos, radius/2, radius/2);
+  }
+  angle += radians(stepSize);
 }
+
